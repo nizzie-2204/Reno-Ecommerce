c59d2c1d2f265831b066eb6d9cd10db0c3c8b657
nizzie-2204 <nguyenanhtuan.220499@gmail.com>
2021-12-08 00:46:15 +0700

    update

--- app.js
+++ app.js
@@ -72,6 +72,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const port = process.env.PORT || 5000
-app.listen(port || 5000, () => {
+app.listen(port, () => {
 	console.log(`Listening: http://localhost:${port}`)
 })
